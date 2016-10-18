bb779fdc36225f4aed7f577a7ac9c90e2d6c3478
fernando <fjanuarioazevedo@gmail.com>
2016-10-17 22:22:01 -0300

    adicionando como chegar

--- home/models.py
+++ home/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 class Home(models.Model):
-	phrase = models.CharField('Frase', max_length=120)
+	phrase = models.CharField('Frase', max_length=120, blank=True)
 	slug = models.SlugField('Identificador',unique=True)
 	image = models.ImageField(upload_to='cumuruxatiba/images', null=True, blank=True)
-	description = models.CharField('Descrição',max_length=120)
+	description = models.CharField('Descrição',max_length=120, blank=True)
 	link = models.CharField('link',max_length=120, blank=True)
 	palavralink = models.CharField('palavra-link',max_length=120, blank=True)
-	subtitle = models.CharField('Legenda', max_length=100)
-	content = models.TextField('Conteudo')
+	subtitle = models.CharField('Legenda', max_length=100, blank=True)
+	content = models.TextField('Conteudo', blank=True)
 	created = models.DateTimeField('Criado em', auto_now_add=True)
 	modified = models.DateTimeField('Modificado em', auto_now=True)
 
